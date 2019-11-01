07626f5d3c9f45d70cbc89c6af614c1e42b44c09
Naren Chejara <naren.chejara@wework.com>
2019-11-01 11:12:29 +0800

    delect unused files

--- dist/simulator.d.ts
+++
@@ -1,7 +0,0 @@
-import { IConnectOptions, ILaunchOptions } from "miniprogram-automator/out/Launcher";
-declare class Simulator {
-    private constructor();
-    static launch(options: ILaunchOptions): Promise<null>;
-    static connect(options: IConnectOptions): Promise<import("miniprogram-automator/out/MiniProgram").default>;
-}
-export default Simulator;
